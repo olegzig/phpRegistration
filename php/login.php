a77ae52b70e2851f6e39518d52e5179f89d0e270
olegzig <boris37544@gmail.com>
2022-09-26 21:24:07 +0300

    try to validate login and email

--- php/login.php
+++ php/login.php
@@ -1,10 +1,11 @@
 <?php
-use Jajo\JSONDB;
 require_once "../vendor/autoload.php";
+use Jajo\JSONDB;
 header('Access-Control-Allow-Origin: *');
 class User
 {
     public $login, $email, $password, $name;
+    private $jsdb;
 
     function __construct($login, $email, $password, $name)
     {
@@ -12,31 +13,78 @@ class User
         $this->login = $login;
         $this->email = $email;
         $this->password = $password;
+        $this->jsdb = new JSONDB('../files/');
     }
 
-    //Проверяем логин на уникальность
+    //Проверяем логин на уникальность. если уникально, возвращает true
     function isLoginUnic()
     {
+        //получем из бд значение
         $jsdb = new JSONDB('../files/');
+        $loginLIstArray = $jsdb->select("login")->from('db.json')->get();
+        $loginLIst = array();
 
+        for($i = 0; $i < count($loginLIstArray); ++$i){
+            $loginLIst[]=$loginLIstArray[$i]["login"];
+        }
+
+        //если запись есть, вернуть false
+        if (in_array($this->login, $loginLIst)) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
-    //Проверяем емейл на уникальность
+    //Проверяем емейл на уникальность. если уникально, возвращает true
     public function isEmailUnic()
     {
-        $jsdb = new JSONDB('../files/');
+        //получем из бд значение
+        $emailLIstArr = $this->jsdb->select('email')->from('db.json')->get();
+        $emailLIst = array();
 
+        for($i = 0; $i < count($emailLIstArr); ++$i){
+            $emailLIst[]=$emailLIstArr[$i]["email"];
+        }
+
+        //если запись есть, вернуть false
+        if (in_array($this->email, $emailLIst) == true) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    function isUnic()
+    {
+        $field = null;
+        if (!$this->isEmailUnic()) {
+            $field[] = 'email';
+        }
+        if (!$this->isLoginUnic()) {
+            $field[] = 'login';
+        }
+
+        if ($field != null) {
+            $response = [
+                "status" => false,
+                "fields" => $field
+            ];
+
+            echo json_encode($response);
+            die();
+        }
     }
     //Создаём акк
     function create()
     {
         //соль = имя
-        $buf = array("name" => $this->name, "login" => $this->login, "email" => $this->email, "password" => md5($this->name.$this->password));
+        $buf = array("name" => $this->name, "login" => $this->login, "email" => $this->email, "password" => md5($this->name . $this->password));
 
-        $this->isEmailUnic();
-        $this->isLoginUnic();
+        $this->isUnic();
 
         $jsdb = new JSONDB('../files/');
-        $jsdb->insert('db.json',$buf);
+        $jsdb->insert('db.json', $buf);
     }
     //Удаляем (не используется, но надо по ТЗ)
     function delete()
@@ -89,5 +137,5 @@ class User
 
 //делаем класс и подлучаем данные с формы
 $user = new User($_POST['login'], $_POST['email'], $_POST['password'], $_POST['name']);
-$user -> create();
+$user->create();
 ?>
